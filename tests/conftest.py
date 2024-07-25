1d363c7170889b6d21dd8cd632ae5b681768212b
PWcislo26 <crouchu26@gmail.com>
2024-07-25 13:56:45 +0200

    add headless option to driver init

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,17 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(params=["chrome"])
 def driver(request):
+
+    options = Options()
+    options.add_argument('--headless')
     browser = request.param
     if browser == "chrome":
-        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     elif browser == "firefox":
         my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     else:
