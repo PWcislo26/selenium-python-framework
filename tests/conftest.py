f8aeb82e241cdaf66dfb0e85f44adbc9b98b1166
PWcislo26 <crouchu26@gmail.com>
2024-07-26 11:13:16 +0200

    refactor options for chrome and firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,21 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions, FirefoxOptions
 
 
 @pytest.fixture(params=["chrome", "firefox"])
 def driver(request):
 
-    options = Options()
-    options.add_argument('--headless')
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless=new")
+    firefox_options = FirefoxOptions()
+    firefox_options.add_argument("--headless")
     browser = request.param
     if browser == "chrome":
-        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     elif browser == "firefox":
-        my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefox_options)
     else:
         raise TypeError(f"Expected 'chrome' or 'firefox', but got {browser}")
     yield my_driver
