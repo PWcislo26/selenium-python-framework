2e32ecf8fe9233d74f8f75f316cdf75c3884ff95
pwcislo26 <crouchu26@gmail.com>
2024-07-28 10:59:54 +0200

    make login page tests use data reader

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -2,6 +2,7 @@ import pytest
 
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
+from utils.data_reader import data_reader
 
 
 class TestLoginPage:
@@ -10,16 +11,16 @@ class TestLoginPage:
     def test_positive_login(self, driver):
         login_page = LoginPage(driver)
         login_page.open()
-        login_page.login(login_page.USERNAME_POSITIVE, login_page.PASSWORD_POSITIVE)
+        login_page.login(data_reader.login_username_standard, data_reader.login_password_correct)
         inventory_page = InventoryPage(driver)
         assert inventory_page.URL == inventory_page.current_url, "Actual URL is not the same as expected"
 
     @pytest.mark.login
     @pytest.mark.parametrize("username, password, expected_error_message",
-                             [(LoginPage.USERNAME_POSITIVE, LoginPage.PASSWORD_NEGATIVE, "Epic sadface: Username and "
+                             [(data_reader.login_username_standard, data_reader.login_password_incorrect, "Epic sadface: Username and "
                                                                                          "password do not match any "
                                                                                          "user in this service"),
-                              (LoginPage.USERNAME_LOCKEDOUT, LoginPage.PASSWORD_POSITIVE, "Epic sadface: Sorry, this "
+                              (data_reader.login_username_lockedout, data_reader.login_password_correct, "Epic sadface: Sorry, this "
                                                                                           "user has been locked out.")])
     def test_negative_login(self, driver, username, password, expected_error_message):
         login_page = LoginPage(driver)
@@ -31,7 +32,7 @@ class TestLoginPage:
     def test_logout(self, driver):
         login_page = LoginPage(driver)
         login_page.open()
-        login_page.login(login_page.USERNAME_POSITIVE, login_page.PASSWORD_POSITIVE)
+        login_page.login(data_reader.login_username_standard, data_reader.login_password_correct)
         inventory_page = InventoryPage(driver)
         inventory_page.logout()
         assert login_page.URL == inventory_page.current_url, "URL different than expected"
