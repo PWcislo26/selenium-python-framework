0091ae002377602a51315f942d88df87ac4901d7
pwcislo26 <crouchu26@gmail.com>
2024-07-28 11:14:40 +0200

    make inventory tests use data reader

--- tests/test_inventory_page.py
+++ tests/test_inventory_page.py
@@ -6,6 +6,7 @@ from pages.checkout_page import CheckoutPage
 from pages.inventory_page import InventoryPage
 from pages.login_page import LoginPage
 from pages.cart_page import CartPage
+from utils.data_reader import data_reader
 
 
 class TestInventoryPage:
@@ -14,7 +15,7 @@ class TestInventoryPage:
     def test_add_product_to_cart(self, driver):
         login_page = LoginPage(driver)
         login_page.open()
-        login_page.login(login_page.USERNAME_POSITIVE, login_page.PASSWORD_POSITIVE)
+        login_page.login(data_reader.login_username_standard, data_reader.login_password_correct)
         inventory_page = InventoryPage(driver)
         inventory_page.add_or_remove_product_from_cart(inventory_page.ADD_BACKPACK_TO_CART_BUTTON)
         assert inventory_page._get_text(
@@ -25,14 +26,15 @@ class TestInventoryPage:
     def test_buy_product(self, driver):
         login_page = LoginPage(driver)
         login_page.open()
-        login_page.login(login_page.USERNAME_POSITIVE, login_page.PASSWORD_POSITIVE)
+        login_page.login(data_reader.login_username_standard, data_reader.login_password_correct)
         inventory_page = InventoryPage(driver)
         inventory_page.add_or_remove_product_from_cart(inventory_page.ADD_BACKPACK_TO_CART_BUTTON)
         inventory_page.open_cart()
         cart_page = CartPage(driver)
         cart_page.checkout()
         checkout_page = CheckoutPage(driver)
-        checkout_page.fill_in_checkout()
+        checkout_page.fill_in_checkout(data_reader.checkout_last_name, data_reader.checkout_last_name,
+                                       data_reader.checkout_postal_code)
         checkout_overview_page = CheckoutOverviewPage(driver)
         checkout_overview_page.finsh_checkout()
         checkout_complete_page = CheckoutCompletePage(driver)
