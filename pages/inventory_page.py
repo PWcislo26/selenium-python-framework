4e36981f8c5a8a06d94a937f303d27930a6e6af2
pwcislo26 <crouchu26@gmail.com>
2024-08-02 12:31:43 +0200

    Move locators and methods to header page

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,17 +1,14 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from pages.header_page import HeaderPage
 
 
-class InventoryPage(BasePage):
+class InventoryPage(HeaderPage):
 
     URL: str = "https://www.saucedemo.com/inventory.html"
     ADD_BACKPACK_TO_CART_BUTTON: tuple = (By.ID, "add-to-cart-sauce-labs-backpack")
     REMOVE_BACKPACK_BUTTON: tuple = (By.ID, "remove-sauce-labs-backpack")
-    SHOPPING_CART: tuple = (By.CLASS_NAME, "shopping_cart_link")
-    SHOPPING_CART_COUNTER: tuple = (By.CLASS_NAME, "shopping_cart_badge")
-    MENU_BUTTON: tuple = (By.ID, "react-burger-menu-btn")
-    LOGOUT_BUTTON: tuple = (By.ID, "logout_sidebar_link")
 
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
@@ -19,17 +16,6 @@ class InventoryPage(BasePage):
     def open(self):
         super()._open_url(self.URL)
 
-    def open_cart(self):
-        super()._click(self.SHOPPING_CART)
-
     def add_or_remove_product_from_cart(self, locator: tuple):
         super()._click(locator)
-
-    def open_menu(self):
-        super()._click(self.MENU_BUTTON)
-
-    def logout(self):
-        self.open_menu()
-        super()._click(self.LOGOUT_BUTTON)
-
 
